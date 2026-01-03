a16872cb44451cca4a5879dbc2f7215df14b5e31
Invalid <justwever@gmail.com>
2026-01-03 22:07:55 +0100

    Updated install-tappy.py to use new filename
    Updated the minified modpack installation script to run the correct file.

--- scripts/install-tappy.bat
+++ scripts/install-tappy.bat
@@ -1,1 +1,1 @@
-iwr -useb https://raw.githubusercontent.com/wasinvalid/tappy-installer/refs/heads/main/scripts/install-tappy.bat -OutFile "$env:TEMP\install-tappy.bat"; cmd /c "$env:TEMP\install-tappy.bat"
+iwr -useb "https://raw.githubusercontent.com/wasinvalid/tappy-installer/refs/heads/main/scripts/modpack-installer.bat" | iex
